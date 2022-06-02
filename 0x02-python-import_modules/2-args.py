4d0c0ff95fa736bb299c949464baedcf76ec5014
Yonashenok <yonashenok24@gmail.com>
2022-06-02 10:55:22 -0700

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    x = len(argv)
-
-    if x - 1  == 0:
-        print(f"{x-1} {'arguments.'}")
-    elif x - 1  == 1:
-        print(f"{x-1} {'argument:'}")
-        print(f"{x-1} {argv[1]}")
-    else:
-        print(f"{x-1} {'arguments:'}")
-        for i in range(len(argv)):
-            if i > 0:
-                print(f"{i}: {argv[i]}")
+    length = len(argv)
+    print("{:d} {:s}{:s}".format(length - 1,
+                                 "argument" if length == 2 else "arguments",
+                                 "." if length == 1 else ":"))
+    for i in range(length):
+        if i > 0:
+            print("{:d}: {:s}".format(i, argv[i]))
