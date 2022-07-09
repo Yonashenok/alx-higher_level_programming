43dd9dc18c6268dff0d4dfcd3bbc9f7bf45c0111
Yonashenok <yonashenok24@gmail.com>
2022-07-09 00:51:06 -0700

    task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-""" defeining function"""
+"""
+Contains the lookup function
+"""
+
+
 def lookup(obj):
-    return (obj)
+    """returns a list of available attributes and methods of an object"""
+    return dir(obj)
